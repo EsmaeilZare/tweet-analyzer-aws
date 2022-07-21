20347ed27177f4f0a2718468f1b0219dbe794bc5
Esmaeil Zare <zare.esmail.1995@gmail.com>
2022-07-21 18:01:29 +0430

    s3 module added

--- config/credentials.py
+++ config/credentials.py
@@ -3,3 +3,4 @@ import decouple
 ACCESS_KEY = decouple.config("ACCESS_KEY")
 SECRET_KEY = decouple.config("SECRET_KEY")
 SESSION_TOKEN = decouple.config("SESSION_TOKEN")
+BUCKET_NAME = decouple.config("BUCKET_NAME")
